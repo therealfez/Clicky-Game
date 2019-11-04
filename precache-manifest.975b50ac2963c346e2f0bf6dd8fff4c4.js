f0542b64ded787b761c6db43ae4ddbab066eae7c
Fauz Ahmed <fauz.ahmed369@gmail.com>
2019-11-04 13:52:20 -0600

    Updates

---
+++ precache-manifest.975b50ac2963c346e2f0bf6dd8fff4c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac399fede3b11f95b11c3f213c6e1888",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "e73aa133f10665d4ce46",
+    "url": "/Clicky-Game/static/css/main.3b316ad7.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/Clicky-Game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "e73aa133f10665d4ce46",
+    "url": "/Clicky-Game/static/js/main.6e8f7e79.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  }
+]);
